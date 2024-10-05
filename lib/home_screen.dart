12ddaecdaa5317fb431bffa116f6ea8c19193ea8
yasmishabana <shabana1.yasmi@gmail.com>
2024-10-05 16:31:14 +0530

    new changes in home page

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fast_food_ordering_ui/custom_widgets/side_heading_text.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,7 +10,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   List foods = ["Burger", "Pizza", "Snacks", "Water"];
-  List food2 = ["Chicken Burger", "Cheese Pizza"];
+  List foods2 = ["Chicken Burger", "Cheese Pizza"];
   List<Color> bgColors = [
     const Color(0xFFFBDCDA),
     const Color(0xFFD4EEF3),
@@ -81,14 +82,284 @@ class _HomeScreenState extends State<HomeScreen> {
                                   width: 3,
                                 ),
                                 shape: BoxShape.circle,
-                                 color: Color(0xFFFF2F08),
+                                color: const Color(0xFFFF2F08),
                                 //child: ,
                               )))
                     ],
                   )
                 ],
               ),
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            // ignore: prefer_const_constructors
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 1.4,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  decoration: BoxDecoration(
+                      color: const Color(0xFFF3F3F3),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Center(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Search your food here....",
+                          border: InputBorder.none,
+                          prefixIcon: Icon(Icons.search),
+                          contentPadding: EdgeInsets.symmetric(vertical: 13)),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {},
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFFF2F08),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: const Icon(
+                      Icons.filter_list,
+                      color: Colors.white,
+                      size: 28,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset("images/banner.jpg"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SideHeadingText(
+                    text: "Categories",
+                  ),
+                  // const Text(
+                  //   "Categories",
+                  //   style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                  // ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "See All",
+                      style: TextStyle(
+                          color: Color(0xFFFF2F08),
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: SizedBox(
+                height: 120,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemCount: foods.length,
+                  itemBuilder: (context, index) {
+                    // Cycle through the custom colors
+                    Color containerColor = bgColors[index % bgColors.length];
+                    return Container(
+                      width: 100,
+                      margin: const EdgeInsets.only(left: 15),
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      decoration: BoxDecoration(
+                          color: containerColor,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Image.asset(
+                            "images/${foods[index]}.png",
+                            height: 80,
+                            width: 80,
+                          ),
+                          Text(
+                            foods[index],
+                            style: const TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black87),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SideHeadingText(
+                    text: "Popular",
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "See All",
+                      style: TextStyle(
+                          color: Color(0xFFFF2F08),
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            SizedBox(
+              height: 230,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: foods2.length,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 1.4,
+                      margin: const EdgeInsets.only(
+                          left: 15, top: 5, right: 5, bottom: 5),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 4,
+                              spreadRadius: 2),
+                        ],
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ClipRRect(
+                            borderRadius: const BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            ),
+                            child: Image.asset(
+                              "images/${foods2[index]}.jpg",
+                              height: 120,
+                              width: MediaQuery.of(context).size.width / 1.4,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 10),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      foods2[index],
+                                      style: const TextStyle(
+                                          fontSize: 17,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black87),
+                                    ),
+                                    const Text(
+                                      "Fast Food",
+                                      style: TextStyle(
+                                          fontSize: 16, color: Colors.black45),
+                                    ),
+                                    const SizedBox(
+                                      height: 5,
+                                    ),
+                                    Row(
+                                      children: [
+                                        Icon(
+                                          Icons.star,
+                                          color: Color(0xFFFF2F08),
+                                          size: 20,
+                                        ),
+                                        SizedBox(width: 2),
+                                        Text(
+                                          "4.7",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(width: 5),
+                                        Text(
+                                          "(941 Ratings)",
+                                          style: TextStyle(
+                                              color: Colors.black45),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  children: [
+                                    const Padding(
+                                      padding: EdgeInsets.all(8),
+                                      child: Row(
+                                        children: [
+                                          Icon(
+                                            Icons.location_on,
+                                            color: Color(0xFFFF2F08),
+                                          ),
+                                          SizedBox(width: 2),
+                                          Text(
+                                            "1 KM",
+                                            style: TextStyle(
+                                                color: Colors.black45,
+                                                fontWeight: FontWeight.w500),
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(height: 10),
+                                    Container(
+                                      padding: const EdgeInsets.all(12),
+                                      decoration: const BoxDecoration(
+                                        color: Color(0xFFFF2F08),
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(10),
+                                        ),
+                                      ),
+                                      child: Text("\$15.89", style: TextStyle(
+                                        fontSize: 16,
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.bold),),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const SizedBox(
+              height:50,
+            ),
           ],
         ),
       ),
