1e5c84586a823991b64946bd62e453c56617dc92
yasmishabana <shabana1.yasmi@gmail.com>
2024-10-05 18:48:22 +0530

    product details page created

---
+++ lib/item_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ItemScreen extends StatefulWidget {
+  const ItemScreen({super.key});
+
+  @override
+  State<ItemScreen> createState() => _ItemScreenState();
+}
+
+class _ItemScreenState extends State<ItemScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
